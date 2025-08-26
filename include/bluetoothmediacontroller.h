021793685690de366680b7b6c0197969abb090c1
Dylan Miller <dmanmiller2004@gmail.com>
2025-08-26 01:29:44 -0400

    Add skeleton implementation of BluetoothMediaController

--- include/bluetoothmediacontroller.h
+++ include/bluetoothmediacontroller.h
@@ -1,15 +1,77 @@
+/*
+    bluetoothmediacontroller.h
+
+    Class declaration for BluetoothMediaController.
+*/
+
 #ifndef BLUETOOTHMEDIACONTROLLER_H
 #define BLUETOOTHMEDIACONTROLLER_H
 
 #include <QObject>
+#include <QtDBus/QtDBus>
+#include <iostream>
 
 class BluetoothMediaController : public QObject
 {
     Q_OBJECT
+
 public:
+    Q_PROPERTY(bool playing READ isPlaying NOTIFY playingChanged)
+    Q_PROPERTY(QString title READ title NOTIFY trackChanged)
+    Q_PROPERTY(QString artist READ artist NOTIFY trackChanged)
+    Q_PROPERTY(QString album READ album NOTIFY trackChanged)
+    Q_PROPERTY(int duration READ duration NOTIFY trackChanged) // in ms
+    Q_PROPERTY(int position READ position NOTIFY positionChanged) // in ms
+    Q_PROPERTY(QString deviceName READ deviceName NOTIFY deviceChanged)
+
+    // ctor
     explicit BluetoothMediaController(QObject *parent = nullptr);
 
+    // Playback controls
+    Q_INVOKABLE void play();
+    Q_INVOKABLE void pause();
+    Q_INVOKABLE void stop();
+    Q_INVOKABLE void next();
+    Q_INVOKABLE void previous();
+    Q_INVOKABLE void seek(int positionMs); // optional
+
+    // Device management
+    Q_INVOKABLE void connectToDevice(const QString &deviceAddress);
+    Q_INVOKABLE void disconnectDevice();
+
+    // Status queries (if needed from C++, mostly properties are enough)
+    bool isPlaying() const;
+    QString title() const;
+    QString artist() const;
+    QString album() const;
+    int duration() const;
+    int position() const;
+    QString deviceName() const;
+
 signals:
+    void playingChanged();
+    void trackChanged();
+    void positionChanged();
+    void deviceChanged();
+    void errorOccurred(const QString &message);
+
+private:
+    QString m_deviceAddress;
+    QString m_deviceName;
+    bool m_playing;
+    QString m_title;
+    QString m_artist;
+    QString m_album;
+    int m_duration;
+    int m_position;
+
+    // DBus interface/proxy objects
+    QDBusInterface *m_mediaPlayerInterface;
+
+    void updatePlaybackStatus();
+    void updateTrackInfo();
+    void handleDbusSignal(...); // callback for DBus properties change
+
 };
 
 #endif // BLUETOOTHMEDIACONTROLLER_H
--- src/bluetoothmediacontroller.cpp
+++ src/bluetoothmediacontroller.cpp
@@ -1,5 +1,102 @@
+/*
+    bluetoothmediacontroller.cpp
+
+    Class definition for BluetoothMediaController.
+*/
+
 #include "bluetoothmediacontroller.h"
 
 BluetoothMediaController::BluetoothMediaController(QObject *parent)
-    : QObject{parent}
-{}
+    : QObject(parent),
+    m_playing(false),
+    m_duration(0),
+    m_position(0)
+{
+    // TODO: Initialize DBus connection and proxies here
+    // For example, connect to system bus and prepare MediaPlayer1 interface
+}
+
+// Playback control methods
+void BluetoothMediaController::play() {
+    std::cout << "Play called" << std::endl;
+    // TODO: Call Play() on the DBus MediaPlayer interface
+    m_playing = true;
+    emit playingChanged();
+}
+
+void BluetoothMediaController::pause() {
+    std::cout << "Pause called" << std::endl;
+    // TODO: Call Pause() on the DBus MediaPlayer interface
+    m_playing = false;
+    emit playingChanged();
+}
+
+void BluetoothMediaController::stop() {
+    std::cout << "Stop called" << std::endl;
+    // TODO: Call Stop() on the DBus MediaPlayer interface
+    m_playing = false;
+    emit playingChanged();
+}
+
+void BluetoothMediaController::next() {
+    std::cout << "Next called" << std::endl;
+    // TODO: Call Next() on the DBus MediaPlayer interface
+}
+
+void BluetoothMediaController::previous() {
+    std::cout << "Previous called" << std::endl;
+    // TODO: Call Previous() on the DBus MediaPlayer interface
+}
+
+void BluetoothMediaController::seek(int positionMs) {
+    std::cout << "Seek called to " << positionMs << " ms" << std::endl;
+    // TODO: Call Seek() on the DBus MediaPlayer interface
+    m_position = positionMs;
+    emit positionChanged();
+}
+
+// Device management
+void BluetoothMediaController::connectToDevice(const QString &deviceAddress) {
+    std::cout << "Connect to device: " << deviceAddress.toStdString() << std::endl;
+    m_deviceAddress = deviceAddress;
+    // TODO: Initialize DBus interface for this device
+    emit deviceChanged();
+}
+
+void BluetoothMediaController::disconnectDevice() {
+    std::cout << "Disconnect device" << std::endl;
+    // TODO: Clear DBus interface and reset state
+    m_deviceAddress.clear();
+    m_playing = false;
+    emit deviceChanged();
+    emit playingChanged();
+}
+
+// Status queries
+bool BluetoothMediaController::isPlaying() const {
+    return m_playing;
+}
+
+QString BluetoothMediaController::title() const {
+    return m_title;
+}
+
+QString BluetoothMediaController::artist() const {
+    return m_artist;
+}
+
+QString BluetoothMediaController::album() const {
+    return m_album;
+}
+
+int BluetoothMediaController::duration() const {
+    return m_duration;
+}
+
+int BluetoothMediaController::position() const {
+    return m_position;
+}
+
+QString BluetoothMediaController::deviceName() const {
+    return m_deviceAddress; // TODO: replace with friendly name if available
+}
