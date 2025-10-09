12fe4121d606db175d9676ef85d4e3eb04c3e316
Dylan Miller <dmanmiller2004@gmail.com>
2025-10-09 14:15:09 -0400

    Skeleton implementation of PulseAudioController

--- include/pulseaudiocontroller.h
+++ include/pulseaudiocontroller.h
@@ -9,10 +9,64 @@ class PulseAudioController : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
+
 public:
+    Q_PROPERTY(bool ready READ isReady NOTIFY readyChanged)
+    Q_PROPERTY(QStringList sinks READ sinks NOTIFY sinksChanged)
+    Q_PROPERTY(int defaultSink READ defaultSink NOTIFY defaultSinkChanged)
+    Q_PROPERTY(qreal defaultSinkVolume READ defaultSinkVolume NOTIFY defaultSinkVolumeChanged)
+    Q_PROPERTY(QStringList sources READ sources NOTIFY sourcesChanged)
+    Q_PROPERTY(int defaultSource READ defaultSource NOTIFY defaultSourceChanged)
+    Q_PROPERTY(qreal defaultSourceVolume READ defaultSourceVolume NOTIFY defaultSourceVolumeChanged)
+
     explicit PulseAudioController(QObject *parent = nullptr);
+    ~PulseAudioController();
+
+    Q_INVOKABLE bool isReady() const;
+    Q_INVOKABLE QStringList sinks() const;
+    Q_INVOKABLE int defaultSink() const;
+    Q_INVOKABLE qreal defaultSinkVolume() const;
+    Q_INVOKABLE QStringList sources() const;
+    Q_INVOKABLE int defaultSource() const;
+    Q_INVOKABLE qreal defaultSourceVolume() const;
+
+    Q_INVOKABLE void setDefaultSink(int index);
+    Q_INVOKABLE void setDefaultSource(int index);
+    Q_INVOKABLE void setDefaultSinkVolume(qreal volume);
+    Q_INVOKABLE void setDefaultSourceVolume(qreal volume);
 
 signals:
+    void readyChanged();
+    void sinksChanged();
+    void defaultSinkChanged();
+    void defaultSinkVolumeChanged();
+    void sourcesChanged();
+    void defaultSourceChanged();
+    void defaultSourceVolumeChanged();
+    void errorOccurred(const QString &message);
+
+private:
+    pa_threaded_mainloop *m_mainloop;
+    pa_context *m_context;
+
+    bool m_ready;
+    QStringList m_sinks;
+    int m_defaultSink;
+    QStringList m_sources;
+    int m_defaultSource;
+
+    static void contextStateCallback(pa_context *c, void *userdata);
+    static void sinkInfoCallback(pa_context *c, const pa_sink_info *i, int eol, void *userdata);
+    static void sourceInfoCallback(pa_context *c, const pa_source_info *i, int eol, void *userdata);
+    static void serverInfoCallback(pa_context *c, const pa_server_info *i, void *userdata);
+
+    void connectPulseAudio();
+    void disconnectPulseAudio();
+    void updateSinks();
+    void updateSources();
+
+private slots:
+    void internalErrorHandle(const QString &message);
 };
 
 #endif // PULSEAUDIOCONTROLLER_H
--- src/pulseaudiocontroller.cpp
+++ src/pulseaudiocontroller.cpp
@@ -1,5 +1,209 @@
 #include "pulseaudiocontroller.h"
+#include <QDebug>
 
 PulseAudioController::PulseAudioController(QObject *parent)
-    : QObject{parent}
-{}
+    : QObject(parent),
+    m_mainloop(nullptr),
+    m_context(nullptr),
+    m_ready(false),
+    m_defaultSink(-1),
+    m_defaultSource(-1)
+{
+    connectPulseAudio();
+}
+
+PulseAudioController::~PulseAudioController()
+{
+    disconnectPulseAudio();
+}
+
+/* -------------------- Connection Management -------------------- */
+
+void PulseAudioController::connectPulseAudio()
+{
+    if (m_ready)
+        return; // already connected
+
+    m_mainloop = pa_threaded_mainloop_new();
+    if (!m_mainloop) {
+        qWarning() << "Failed to create PulseAudio mainloop";
+        emit errorOccurred("Failed to create PulseAudio mainloop");
+        return;
+    }
+
+    pa_mainloop_api *api = pa_threaded_mainloop_get_api(m_mainloop);
+    m_context = pa_context_new(api, "QtPulseAudioController");
+
+    if (!m_context) {
+        qWarning() << "Failed to create PulseAudio context";
+        emit errorOccurred("Failed to create PulseAudio context");
+        pa_threaded_mainloop_free(m_mainloop);
+        m_mainloop = nullptr;
+        return;
+    }
+
+    pa_context_set_state_callback(m_context, &PulseAudioController::contextStateCallback, this);
+
+    if (pa_context_connect(m_context, nullptr, PA_CONTEXT_NOFLAGS, nullptr) < 0) {
+        qWarning() << "Failed to connect to PulseAudio server";
+        emit errorOccurred("Failed to connect to PulseAudio server");
+        disconnectPulseAudio();
+        return;
+    }
+
+    if (pa_threaded_mainloop_start(m_mainloop) < 0) {
+        qWarning() << "Failed to start PulseAudio mainloop";
+        emit errorOccurred("Failed to start PulseAudio mainloop");
+        disconnectPulseAudio();
+        return;
+    }
+
+    // Successfully initiated connection
+    m_ready = true;
+    emit readyChanged();
+
+    // TODO: populate initial sinks and sources
+    updateSinks();
+    updateSources();
+}
+
+void PulseAudioController::disconnectPulseAudio()
+{
+    if (m_context) {
+        pa_context_disconnect(m_context);
+        pa_context_unref(m_context);
+        m_context = nullptr;
+    }
+
+    if (m_mainloop) {
+        pa_threaded_mainloop_stop(m_mainloop);
+        pa_threaded_mainloop_free(m_mainloop);
+        m_mainloop = nullptr;
+    }
+
+    m_ready = false;
+    m_sinks.clear();
+    m_sources.clear();
+    m_defaultSink = -1;
+    m_defaultSource = -1;
+
+    emit readyChanged();
+    emit sinksChanged();
+    emit sourcesChanged();
+    emit defaultSinkChanged();
+    emit defaultSourceChanged();
+}
+
+/* -------------------- Getters -------------------- */
+
+bool PulseAudioController::isReady() const
+{
+    return m_ready;
+}
+
+QStringList PulseAudioController::sinks() const
+{
+    return m_sinks;
+}
+
+int PulseAudioController::defaultSink() const
+{
+    return m_defaultSink;
+}
+
+qreal PulseAudioController::defaultSinkVolume() const
+{
+    return 0.0; // stub
+}
+
+QStringList PulseAudioController::sources() const
+{
+    return m_sources;
+}
+
+int PulseAudioController::defaultSource() const
+{
+    return m_defaultSource;
+}
+
+qreal PulseAudioController::defaultSourceVolume() const
+{
+    return 0.0; // stub
+}
+
+/* -------------------- Setters -------------------- */
+
+void PulseAudioController::setDefaultSink(int index)
+{
+    Q_UNUSED(index);
+    // stub
+}
+
+void PulseAudioController::setDefaultSource(int index)
+{
+    Q_UNUSED(index);
+    // stub
+}
+
+void PulseAudioController::setDefaultSinkVolume(qreal volume)
+{
+    Q_UNUSED(volume);
+    // stub
+}
+
+void PulseAudioController::setDefaultSourceVolume(qreal volume)
+{
+    Q_UNUSED(volume);
+    // stub
+}
+
+/* -------------------- Callbacks & Internal -------------------- */
+
+void PulseAudioController::contextStateCallback(pa_context *c, void *userdata)
+{
+    Q_UNUSED(c);
+    Q_UNUSED(userdata);
+    // stub
+}
+
+void PulseAudioController::sinkInfoCallback(pa_context *c, const pa_sink_info *i, int eol, void *userdata)
+{
+    Q_UNUSED(c);
+    Q_UNUSED(i);
+    Q_UNUSED(eol);
+    Q_UNUSED(userdata);
+    // stub
+}
+
+void PulseAudioController::sourceInfoCallback(pa_context *c, const pa_source_info *i, int eol, void *userdata)
+{
+    Q_UNUSED(c);
+    Q_UNUSED(i);
+    Q_UNUSED(eol);
+    Q_UNUSED(userdata);
+    // stub
+}
+
+void PulseAudioController::serverInfoCallback(pa_context *c, const pa_server_info *i, void *userdata)
+{
+    Q_UNUSED(c);
+    Q_UNUSED(i);
+    Q_UNUSED(userdata);
+    // stub
+}
+
+void PulseAudioController::updateSinks()
+{
+    // stub
+}
+
+void PulseAudioController::updateSources()
+{
+    // stub
+}
+
+void PulseAudioController::internalErrorHandle(const QString &message)
+{
+    qWarning() << "PulseAudio error:" << message;
+    emit errorOccurred(message);
+}
